d2c7f487aac14f107e377453e69ac450c6e21efb
xz32 <11448696+xz32@users.noreply.github.com>
2023-04-12 16:46:06 +0100

    Token generation

---
+++ tokens/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getDatabase } from 'firebase-admin/database';
+
+import { v4 as uuidv4 } from 'uuid';
+
+const admin = initializeApp({
+    databaseURL: "https://sta-cs5041-p4.firebaseio.com"
+});
+const database = getDatabase(admin);
+
+const authRef = database.ref(`auth`);
+
+new Array(50).fill(null).map(el => uuidv4()).forEach(el => {
+    authRef.child(el).set({
+        note: 'generated'
+    });
+    console.log(el);
+});
