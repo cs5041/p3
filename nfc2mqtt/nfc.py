d8114153941858b0e3afb2fdc8be4f03a986b464
xz32 <11448696+xz32@users.noreply.github.com>
2023-11-13 18:17:32 +0000

    nfc to mqtt

--- nfc2mqtt/nfc.py
+++ nfc2mqtt/nfc.py
@@ -9,7 +9,31 @@ import RPi.GPIO as GPIO
 
 from pn532 import *
 
+import paho.mqtt.client as mqtt
 
+import base64
+
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
+mqttpass = ""
+with open("mqttpass") as password_file:
+    mqttpass = password_file.read().strip(" \n\r")
+
+salt = ""
+with open("salt") as salt_file:
+    salt = salt_file.read().strip(" \n\r")
+    salt = base64.b64decode(salt) 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.username_pw_set("cs5041", mqttpass)
+
+client.connect("cs5041", 1883, 60)
+
+client.loop_start()
 
 try:
     pn532 = PN532_SPI(debug=False, reset=20, cs=4)
@@ -26,11 +50,20 @@ try:
     while True:
         # Check if a card is available to read
         uid = pn532.read_passive_target(timeout=0.5)
-        print('.', end="")
         # Try again if no card is available.
         if uid is None:
             continue
-        print('Found card with UID:', [hex(i) for i in uid])
+        kdf = Scrypt(
+            salt=salt,
+            length=32,
+            n=2**14,
+            r=8,
+            p=1,
+        )
+        key = kdf.derive(''.join([hex(i) for i in uid]))
+        key = key.hex()
+        print('Found card with hashed UID:', key))
+        client.publish('nfc2mqtt', key)
 except Exception as e:
     print(e)
 finally:
